d4bc881b9466201ea04a3792a4dbef39e8072db7
Mahardhika Bredy Dwi Gagah Santoso <bredymahardhika@gmail.com>
2024-07-26 23:21:26 +0700

    Update Page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,21 @@
 import Image from "next/image";
 
+import Headline from "@/components/Headline";
 import BeritaPopuler from "@/components/BeritaPopuler";
+import Rekomendasi from "@/components/Rekomendasi";
+import Slider from "@/components/Slider";
 
 export default function Home() {
   return (
-    <main>
-      <div className="h-screen">
+    <main className="bg-white">
+      <div>
+        <Headline />
+        <div className="h-20"></div>
         <BeritaPopuler />
+        <div className="h-20"></div>
+        <Rekomendasi />
+        <div className="h-20"></div>
+        <Slider />
       </div>
     </main>
   );
